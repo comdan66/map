281c49abcb020643d51b17616439a458724e77ef
OA Wu <comdan66@gmail.com>
2015-08-01 22:36:19 +0800

    Create migration and model.

---
+++ application/migrations/001_add_events.php
@@ -0,0 +1,27 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 OA Wu Design
+ */
+
+class Migration_Add_events extends CI_Migration {
+  public function up () {
+    $this->db->query (
+      "CREATE TABLE `events` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT '名稱',
+        `description` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT '敘述',
+        `cover` varchar(100) COLLATE utf8_unicode_ci NOT NULL COMMENT '封面',
+        `updated_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '更新時間',
+        `created_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '新增時間',
+        PRIMARY KEY (`id`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
+    );
+  }
+  public function down () {
+    $this->db->query (
+      "DROP TABLE `events`;"
+    );
+  }
+}
