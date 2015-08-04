da9dab9538daaefbf1ec0b72c396057235580f26
OA Wu <comdan66@gmail.com>
2015-08-04 23:26:27 +0800

    Fix code.

--- application/views/content/site/main/index/content.php
+++ application/views/content/site/main/index/content.php
@@ -4,12 +4,12 @@
 
 <?php
     foreach ($events as $event) { ?>
-      <a href='' class='unit'>
+      <a href='<?php echo base_url ('event', $event->id);?>' class='unit'>
           <img src='<?php echo $event->cover->url ('400x400c');?>' alt='<?php echo $event->name;?>'/>
           <div class='title'><?php echo $event->name;?></div>
           <div class='bottom'>
-            <div class='l'><?php echo round ($event->length / 1000, 2);?></div>
-            <div class='r'><?php echo $event->run_time_str (); ?></div>
+            <div class='l'>總長度：<?php echo round ($event->length / 1000, 2);?> Km</div>
+            <div class='r'>歷經：<?php echo $event->run_time_str (); ?></div>
           </div>
         </a>
   <?php
