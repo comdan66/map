dcfd1a6b6a55a1c0a7139089d7e0361b5e86317e
OA Wu <comdan66@gmail.com>
2015-12-20 19:41:20 +0800

    Temp

---
+++ old/application/config/setting/google.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 OA Wu Design
+ */
+
+$google['development'] = array (
+    'client_key' => 'AIzaSyB6guB5HDAcAdmOh6MOQTEh5M84JbWC7o4',
+    'server_key' => '',
+  );
+$google['staging'] = array (
+    'client_key' => '',
+    'server_key' => '',
+  );
+$google['production'] = array (
+    'client_key' => 'AIzaSyB6guB5HDAcAdmOh6MOQTEh5M84JbWC7o4',
+    'server_key' => '',
+  );
+$google['client_js_url'] = 'https://maps.googleapis.com/maps/api/js?key=' . $google[ENVIRONMENT]['client_key'] . '&v=3.exp&sensor=false&language=zh-TW';
