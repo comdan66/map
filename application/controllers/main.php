2b5e1f33432a4fdcbbe10ec52919a9ffc392d3e0
OA Wu <comdan66@gmail.com>
2015-12-22 22:57:54 +0800

    Add composer, push sample.

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,4 +1,17 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+use Parse\ParseObject;
+use Parse\ParseQuery;
+use Parse\ParseACL;
+use Parse\ParsePush;
+use Parse\ParseUser;
+use Parse\ParseInstallation;
+use Parse\ParseException;
+use Parse\ParseAnalytics;
+use Parse\ParseFile;
+use Parse\ParseCloud;
+use Parse\ParseClient;
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
  * @author      OA Wu <comdan66@gmail.com>
@@ -7,6 +20,26 @@
 
 class Main extends Site_controller {
 
+  public function x () {
+    require FCPATH . 'vendor/autoload.php';
+
+    ParseClient::initialize (Cfg::setting ('parse', ENVIRONMENT, 'app_id'), Cfg::setting ('parse', ENVIRONMENT, 'rest_key'), Cfg::setting ('parse', ENVIRONMENT, 'master_key'));
+
+    $data = array (
+            "sound" => "default",
+            'alert' => '歐Ａ人現在在永和喲！',
+            'badge' => 0
+            // 'launch-image' => 'http://www.imageshop.com.tw/pic/shop/home/women-world.jpg'
+        );
+    // $query = ParseInstallation::query();
+
+    ParsePush::send (array (
+        // 'where' => $query,
+        "channels" => ["user_1"],
+        "data" => $data
+    ));
+
+  }
   private function _post ($url, $fields = array ()) {
     $options = array (
       CURLOPT_URL => $url, CURLOPT_POST => true,
